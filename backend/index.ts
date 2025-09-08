dbce927983fea11fbf705cb20d012d2628a3ba19
mnayeembasha <mnayeembasha@gmail.com>
2025-09-08 22:38:46 +0530

    add products component

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { PORT } from "./config";
 import cors from "cors";
+import path from "path";
 const app = express();
 import productRoutes from "./routes/product.routes";
 import { connectDB } from "./lib/db";
+import dotenv from "dotenv";
+dotenv.config();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -14,6 +17,14 @@ app.use(express.json({limit:"5MB"}));
 app.use(express.urlencoded({extended:true,limit:"5MB"}));
 
 app.use("/api/products",productRoutes);
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get(/.*/,(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,9 @@
-export default function App() {
-    return <div>ProdManager - Product Management Application</div>;
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import {Product} from "./components/products/Products";
+
+export default function App () {
+    return (<Routes>
+        <Route path="/products" element={<Product/>}/>
+    </Routes>);
 }
